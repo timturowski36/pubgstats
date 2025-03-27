99b3705a153a2b795b2cdfca2ab39f77b54dfd5c
TimTurowski <turowski1207@web.de>
2025-03-27 16:40:38 +0100

    feat: Unterfunktionen zum abrufen der Accountinformationen in main.py hinterlegt

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ import os
 from scripts.psn_win_parser import fetch_and_write_psn_wins
 from scripts.steam_win_parser import fetch_and_write_steam_wins
 from scripts.win_kd_parser_12h import write_12h_summary
+from scripts.account_id_fetcher import fetch_and_save_account_id
 
 # 🚀 Hauptfunktion
 print("Starte PUBG-Stats-Monitor. Alle 2 Minuten wird aktualisiert.\nDrücke [Strg + C] zum Beenden.")
 while True:
+    fetch_and_save_account_id("brotrustgaming", "steam")
+    fetch_and_save_account_id("brotrustgaming", "psn")
     fetch_and_write_psn_wins()
     fetch_and_write_steam_wins()
     write_12h_summary()
